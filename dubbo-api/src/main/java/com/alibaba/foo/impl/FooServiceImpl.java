5b8be16871f6b6aa78c440fe2a608d07914aecff
Tiandaguang <zhizunwudivip2000@163.com>
2018-04-27 18:51:49 +0800

    去除dubbo-admin

--- dubbo-api/src/main/java/com/alibaba/foo/impl/FooServiceImpl.java
+++ dubbo-api/src/main/java/com/alibaba/foo/impl/FooServiceImpl.java
@@ -7,14 +7,13 @@ import com.alibaba.foo.FooService;
 @Service("fooServiceImpl")
 public class FooServiceImpl implements FooService {
 
-	@Override
-	public String findFoo() {
-		try {
-			Thread.sleep(3000l);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return "fooServiceImpl";
-	}
+    public String findFoo() {
+        try {
+            Thread.sleep(3000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "fooServiceImpl";
+    }
 
 }
